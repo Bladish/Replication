28180a885862d5035d4811765f68ecde34c9ca84
HongKong <pajo0043@gmail.com>
2020-05-12 12:05:11 +0200

    PLay Area

--- Replication/Assets/Scripts/AI/States/PatrolState.cs
+++ Replication/Assets/Scripts/AI/States/PatrolState.cs
@@ -63,7 +63,7 @@ public class PatrolState : BaseState
     private void FindRandomDestination()
     {
         Vector3 testPosition = (_transform.position + (_transform.forward * 4f))
-            + new Vector3(UnityEngine.Random.Range(-4.5f, 4.5f), 0f, UnityEngine.Random.Range(-4.5f, 4.5f));
+            + new Vector3(UnityEngine.Random.Range(-4.5f, 4.5f), UnityEngine.Random.Range(-4.5f, 4.5f), UnityEngine.Random.Range(-4.5f, 4.5f));
         _destination = new Vector3(testPosition.x, testPosition.y, testPosition.z);
         _direction = Vector3.Normalize(_destination.Value - _transform.position);
         _direction = new Vector3(_direction.x, _direction.y, _direction.z);
